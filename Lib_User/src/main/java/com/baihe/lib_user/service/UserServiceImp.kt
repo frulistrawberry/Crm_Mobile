49c5b685511346a8471ea8d3e3cde6bf13f1cdba
baoyandi <baoyandi@jiayuan.com>
2023-07-14 16:30:22 +0800

    创建获取我的页面的方法

--- Lib_User/src/main/java/com/baihe/lib_user/service/UserServiceImp.kt
+++ Lib_User/src/main/java/com/baihe/lib_user/service/UserServiceImp.kt
@@ -1,6 +1,7 @@
 package com.baihe.lib_user.service
 
 import android.content.Context
+import androidx.fragment.app.Fragment
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.baihe.http.EasyConfig
 import com.baihe.lib_common.constant.RoutePath.USER_SERVICE_USER
@@ -12,37 +13,38 @@ import com.baihe.lib_framework.storage.StorageManager
 import com.baihe.lib_user.constant.CacheConstant
 import com.baihe.lib_user.constant.UrlConstant
 import com.baihe.lib_user.dialog.BossSeaDialog
+import com.baihe.lib_user.ui.MineFragment
 
 @Route(path = USER_SERVICE_USER)
-class UserServiceImp:IUserService {
+class UserServiceImp : IUserService {
     override fun saveUserInfo(user: UserEntity?) {
-        StorageManager.putObject(CacheConstant.USER_INFO_DATA,user)
+        StorageManager.putObject(CacheConstant.USER_INFO_DATA, user)
         StorageManager.put(CacheConstant.USER_PHONE_NUMBER, user?.account)
         StorageManager.put(CacheConstant.USER_ID, user?.id.toString())
         StorageManager.put(CacheConstant.USER_COMPANY_ID, user?.company_id.toString())
         StorageManager.put(CacheConstant.USER_COMPANY_NAME, user?.company_name.toString())
-        EasyConfig.getInstance().addParam("userId",user?.id.toString())
-        EasyConfig.getInstance().addParam("companyId",user?.company_id.toString())
+        EasyConfig.getInstance().addParam("userId", user?.id.toString())
+        EasyConfig.getInstance().addParam("companyId", user?.company_id.toString())
     }
 
     override fun getUserInfo(): UserEntity? {
-        return StorageManager.getObject(CacheConstant.USER_INFO_DATA,UserEntity::class.java)
+        return StorageManager.getObject(CacheConstant.USER_INFO_DATA, UserEntity::class.java)
     }
 
     override fun getUserId(): String {
-        return StorageManager.get(CacheConstant.USER_ID,"") as String
+        return StorageManager.get(CacheConstant.USER_ID, "") as String
     }
 
     override fun getPhoneNum(): String? {
-        return StorageManager.get(CacheConstant.USER_PHONE_NUMBER,"") as String?
+        return StorageManager.get(CacheConstant.USER_PHONE_NUMBER, "") as String?
     }
 
     override fun getCompanyId(): String {
-        return  StorageManager.get(CacheConstant.USER_COMPANY_ID,"") as String
+        return StorageManager.get(CacheConstant.USER_COMPANY_ID, "") as String
     }
 
     override fun getCompanyName(): String? {
-        return  StorageManager.get(CacheConstant.USER_COMPANY_NAME,"") as String
+        return StorageManager.get(CacheConstant.USER_COMPANY_NAME, "") as String
     }
 
     override fun clearUserInfo() {
@@ -54,12 +56,13 @@ class UserServiceImp:IUserService {
     }
 
     override fun readAgreement(context: Context) {
-        WebActivity.start(context,"用户协议",UrlConstant.AGREEMENT_LIHE)
+        WebActivity.start(context, "用户协议", UrlConstant.AGREEMENT_LIHE)
     }
 
     override fun readPolicy(context: Context) {
-        WebActivity.start(context,"隐私政策",UrlConstant.PRIVACY_POLICY)
+        WebActivity.start(context, "隐私政策", UrlConstant.PRIVACY_POLICY)
     }
+
 
     override fun getBossSeaDialog(
         context: Context,
@@ -73,5 +76,13 @@ class UserServiceImp:IUserService {
 
     override fun init(context: Context?) {
 
+    }
+
+    override fun toMine(context: Context) {
+
+    }
+
+    override fun getMineFragment(): Fragment {
+        return MineFragment()
     }
 }
